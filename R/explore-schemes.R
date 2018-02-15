a6e23527ead0c4d697e553d215487396c66f49c4
Duncan Garmonsway <nacnudus@gmail.com>
2018-02-15 12:34:15 +0000

    Note differences between 2013/14/15 and 2015/16/17 sources

--- R/explore-schemes.R
+++ R/explore-schemes.R
@@ -10,21 +10,34 @@ library(tidyverse)
 library(scales)
 library(here)
 
-source_url <- "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/655239/GGIS_Grant_Schemes_2016_to_2017.csv"
+path201314 <- here("lists", "government-grant-schemes-2013-14.csv")
+path201415 <- here("lists", "government-grant-schemes-2014-15.csv")
+path201516 <- here("lists", "government-grant-schemes-2015-16.csv")
+path201617 <- here("lists", "government-grant-schemes-2016-17.csv")
 
-source_data <- read_csv(source_url)
+scheme201314 <- read_csv(path201314)
+scheme201415 <- read_csv(path201415)
+scheme201516 <- read_csv(path201516)
+scheme201617 <- read_csv(path201617)
 
-write_tsv(source_data, here("lists", "government-grant-scheme-source.tsv"))
+#'
+#' The 2013/14 and 2014/15 years
+#'
+#' * give the number of recipients
+#' * don't have grant IDs
+#' * use different monetary units (£,000s and £,000,000s)
+#'
+#' ## 2016/17 schemes
+#'
+#' All seem to be in the government-organisation register,
+#' Though some names have changed
+count(scheme201617, `Funding Org:Name`)
 
-# All seem to be in the government-organisation register,
-# Though some names have changed
-count(source_data, `Funding Org:Name`)
+#' `Allocation Method` is cardinality='n'
+distinct(scheme201617, `Allocation Method`)
 
-# `Allocation Method` is cardinality='n'
-distinct(source_data, `Allocation Method`)
-
-# There are only four `Allocation Method`s available
-distinct(source_data, `Allocation Method`) %>%
+#' There are only four `Allocation Method`s available
+distinct(scheme201617, `Allocation Method`) %>%
   transmute(method = map(`Allocation Method`, str_split, pattern = "; "),
             method = map(method, unlist)) %>%
   unnest() %>%
@@ -33,11 +46,11 @@ distinct(source_data, `Allocation Method`) %>%
   print() %>%
   write_tsv(here("lists", "government-grant-allocation-method.tsv"))
 
-# `Recipient Sector` is cardinality='n'
-distinct(source_data, `Recipient Sector`)
+#' `Recipient Sector` is cardinality='n'
+distinct(scheme201617, `Recipient Sector`)
 
-# There are only eight `Recipient Sector`s available
-distinct(source_data, `Recipient Sector`) %>%
+#' There are only eight `Recipient Sector`s available
+distinct(scheme201617, `Recipient Sector`) %>%
   transmute(sector = map(`Recipient Sector`, str_split, pattern = "; "),
             sector = map(sector, unlist)) %>%
   unnest() %>%
@@ -46,55 +59,41 @@ distinct(source_data, `Recipient Sector`) %>%
   print() %>%
   write_tsv(here("lists", "government-grant-recipient-sector.tsv"))
 
-# What is `COFOG L0 Long Name`? Another potential register?j
-count(source_data, `COFOG L0 Long Name`)
+#' What is `COFOG L0 Long Name`? Another potential register?j
+count(scheme201617, `COFOG L0 Long Name`)
 
-# Durations in years.  Is 20 a legislative maximum?
-count(source_data, `Duration Numeric`)
+#' Durations in years.  Is 20 a legislative maximum?
+count(scheme201617, `Duration Numeric`)
 
-# The top six aims and objectives of government grants
-slice(count(source_data, `Aims and objectives`, sort = TRUE), 1:6)
-# # A tibble: 6 x 2
-#   `Aims and objectives`                                                       n
-# 1 To reduce global poverty in support of the UK_s national interest. Fur…   393
-# 2 <NA>                                                                      200
-# 3 .                                                                          81
-# 4 FCO Policy Objectives                                                      24
-# 5 TBC Q1 2017/18                                                             21
-# 6 n/a                                                                        18
+#' The top six aims and objectives of government grants
+slice(count(scheme201617, `Aims and objectives`, sort = TRUE), 1:6)
 
-pull(slice(count(source_data, `Aims and objectives`, sort = TRUE), 1:6), 1)
-# [1] "To reduce global poverty in support of the UK_s national interest. Further details on all UK overseas development assistance is available on Development Tracker at https://devtracker.dfid.gov.uk/"
-# [2] NA
-# [3] "."
-# [4] "FCO Policy Objectives"
-# [5] "TBC Q1 2017/18"
-# [6] "n/a"
+pull(slice(count(scheme201617, `Aims and objectives`, sort = TRUE), 1:6), 1)
 
-# The unique ID is the `Grant Programme:Code`
-count(source_data, `Grant Programme:Code`, sort = TRUE)
+#' The unique ID is the `Grant Programme:Code`
+count(scheme201617, `Grant Programme:Code`, sort = TRUE)
 
-# Some values are zero
-count(source_data, `Value per year`, sort = TRUE)
+#' Some values are zero
+count(scheme201617, `Value per year`, sort = TRUE)
 
-# Some values are negative, some are not really grants (Crossrail?!)
-count(source_data, `Value per year`)
-arrange(source_data, `Value per year`) %>%
+#' Some values are negative, some are not really grants (Crossrail?!)
+count(scheme201617, `Value per year`)
+arrange(scheme201617, `Value per year`) %>%
   select(1:3, 5)
 
-count(source_data, `Value per year`) %>%
+count(scheme201617, `Value per year`) %>%
   arrange(-`Value per year`)
 
 pound <- dollar_format(prefix = "£")
 
-source_data %>%
+scheme201617 %>%
   count(`Value per year`) %>%
   ggplot(aes(x = `Value per year`, y = n)) +
   geom_point() +
   scale_x_log10(label = pound) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-source_data %>%
+scheme201617 %>%
   ggplot(aes(x = `Value per year`)) +
   geom_density() +
   scale_x_log10(label = pound) +
