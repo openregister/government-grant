e79c10380dba054d188fb2ea910d3f0e3c6b027a
Duncan Garmonsway <nacnudus@gmail.com>
2018-02-15 12:33:59 +0000

    Fix source file encoding glitches

--- R/download-sources.R
+++ R/download-sources.R
@@ -6,6 +6,14 @@ download.file("https://www.gov.uk/government/uploads/system/uploads/attachment_d
 download.file("https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/655237/GGIS_Grant_Schemes_2015_to_2016.csv",
               "lists/government-grant-schemes-2015-16.csv")
 download.file("https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/502415/Grants_Register.February16.csv",
-              "lists/government-grant-schemes-2014-15.csv")
+              "lists/government-grant-schemes-2014-15-dirty.csv")
 download.file("https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/405234/2013-14_Government_Grants.csv",
-              "lists/government-grant-schemes-2013-14.csv")
+              "lists/government-grant-schemes-2013-14-dirty.csv")
+
+system("sed -i \"s/\\r/\\n/g\" lists/government-grant-schemes-2014-15-dirty.csv")
+system("sed -i \"s/\\r/\\n/g\" lists/government-grant-schemes-2013-14-dirty.csv")
+system("iconv -f ISO-8859-1 -t UTF-8 -o lists/government-grant-schemes-2013-14.csv lists/government-grant-schemes-2013-14-dirty.csv")
+system("iconv -f ISO-8859-1 -t UTF-8 -o lists/government-grant-schemes-2014-15.csv lists/government-grant-schemes-2014-15-dirty.csv")
+
+file.remove("lists/government-grant-schemes-2013-14-dirty.csv")
+file.remove("lists/government-grant-schemes-2014-15-dirty.csv")
